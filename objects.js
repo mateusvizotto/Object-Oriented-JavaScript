0a4c055e4d6410d18c89c47bb2e8ff0b9d2b38eb
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2020-06-24 12:23:31 -0300

    4 - Classes

--- objects.js
+++ objects.js
@@ -1,32 +1,3 @@
-var userOne = {
-    email: 'ryu@ninjas.com',
-    name: 'Ryu',
-    login() {
-        console.log(this.email, 'has logged in');
-    },
-    logout() {
-        console.log(this.email, 'has logged out');
-    }
-};
+class User {
 
-var userTwo = {
-    email: 'yoshi@ninjas.com',
-    name: 'Yoshi',
-    login() {
-        console.log(this.email, 'has logged in');
-    },
-    logout() {
-        console.log(this.email, 'has logged out');
-    }
-};
-
-var userThree = {
-    email: 'mario@ninjas.com',
-    name: 'Mario',
-    login() {
-        console.log(this.email, 'has logged in');
-    },
-    logout() {
-        console.log(this.email, 'has logged out');
-    }
-};
+}
