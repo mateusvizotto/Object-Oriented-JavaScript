6d5ac2754346571767b4d55a6042a2333a4d7e5b
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2020-06-24 12:35:11 -0300

    5 - Class Constructors

--- objects.js
+++ objects.js
@@ -1,3 +1,17 @@
 class User {
-
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
 }
+
+var userOne = new User('ryu@ninjas.com', 'Ryu');
+var userTwo = new User('yoshi@ninjas.com', 'Yoshi');
+
+console.log(userOne);
+console.log(userTwo);
+
+// the 'new' keyword
+// - creates a new empty object
+// - sets the value of 'this' to be the new empty object
+// - calls the constructor method
