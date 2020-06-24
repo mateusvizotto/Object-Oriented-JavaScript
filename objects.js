2123c465dd1958ad1cd498cd85fa5aab73bc6038
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2020-06-24 12:17:31 -0300

    3 - Updating Properties

--- objects.js
+++ objects.js
@@ -9,3 +9,24 @@ var userOne = {
     }
 };
 
+var userTwo = {
+    email: 'yoshi@ninjas.com',
+    name: 'Yoshi',
+    login() {
+        console.log(this.email, 'has logged in');
+    },
+    logout() {
+        console.log(this.email, 'has logged out');
+    }
+};
+
+var userThree = {
+    email: 'mario@ninjas.com',
+    name: 'Mario',
+    login() {
+        console.log(this.email, 'has logged in');
+    },
+    logout() {
+        console.log(this.email, 'has logged out');
+    }
+};
