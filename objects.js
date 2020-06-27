a08668fa437546c2878609c1a85230f7554de5eb
Mateus Braun Vizotto <mateusbraunvizotto@gmail.com>
2020-06-27 19:52:59 -0300

    11 - Prototype Inheritance

--- objects.js
+++ objects.js
@@ -14,11 +14,26 @@ User.prototype.logout = function () {
     console.log(this.email, 'has logged out');
 }
 
+function Admin(...args) {
+    User.apply(this, args);
+    this.role = 'super admin';
+}
+
+Admin.prototype = Object.create(User.prototype);
+
+Admin.prototype.deleteUser = function (u) {
+    users = users.filter(user => {
+        return user.email != u.email;
+    });
+};
+
 var userOne = new User('ryu@ninjas.com', 'Ryu');
 var userTwo = new User('yoshi@ninjas.com', 'Yoshi');
+var admin = new Admin('mario@ninjas.com', 'Mario');
 
-console.log(userOne);
-userTwo.login();
+var users = [userOne, userTwo, admin];
+
+console.log(admin);
 
 /*
 class User {
